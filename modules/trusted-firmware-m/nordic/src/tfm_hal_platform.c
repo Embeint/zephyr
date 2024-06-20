cb66f0979c4aa54a93d22ef3c608de771288f944
Jordan Yates <jordan@embeint.com>
2024-06-20 15:41:03 +1000

    modules: tfm: cc3xx_platform entropy
    Use the CC3XX platform library as a hardware entropy source for TF-M,
    instead of the insecure `MBEDTLS_ENTROPY_NV_SEED`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- modules/trusted-firmware-m/nordic/src/tfm_hal_platform.c
+++ modules/trusted-firmware-m/nordic/src/tfm_hal_platform.c
@@ -7,7 +7,16 @@
 #include "tfm_hal_defs.h"
 #include "tfm_hal_platform_common.h"
 
+#ifdef NRF_CC3XX_PLATFORM_LIB
+#include "nrf_cc3xx_platform.h"
+#endif
+
 enum tfm_hal_status_t tfm_hal_platform_init(void)
 {
+#ifdef NRF_CC3XX_PLATFORM_LIB
+	if (nrf_cc3xx_platform_init() != 0) {
+		return TFM_HAL_ERROR_GENERIC;
+	}
+#endif
 	return tfm_hal_platform_common_init();
 }
