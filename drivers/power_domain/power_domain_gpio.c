41e5e2eb3f892bea3360979bd941bd32fe546c4b
Jordan Yates <jordan@embeint.com>
2024-07-16 17:22:42 +1000

    power_domain: gpio: raw follower GPIOs
    Extra list of GPIOs that 'follow' the state of the power domain.
    These GPIOs are set to physical level '0' when the domain transitions
    to SUSPENDED, and set to physical level '1' when the domain transitions
    to ACTIVE. The logical levels (ACTIVE_HIGH/ACTIVE_LOW) are not modified
    by this action (through gpio_pin_configure).
    
    This behaviour is intended to be used when an SPI device is on a power
    domain and the CS pin is explicitly driven high and low by the SoC,
    instead of having an external pull-up to the voltage rail. The idle
    state of the CS pin needs to be high, but when the power domain goes
    down the CS pin needs to go low to avoid back-powering the device.
    
    This property is NOT needed when the CS pins have an external pullup
    to the voltage rail that is being switched.
    
    The BMI270 sensor on the Nordic Thingy53 is an example of this hardware
    setup.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/power_domain/power_domain_gpio.c
+++ drivers/power_domain/power_domain_gpio.c
@@ -17,6 +17,8 @@ LOG_MODULE_REGISTER(power_domain_gpio, CONFIG_POWER_DOMAIN_LOG_LEVEL);
 
 struct pd_gpio_config {
 	struct gpio_dt_spec enable;
+	const struct gpio_dt_spec *followers;
+	uint32_t num_followers;
 	uint32_t startup_delay_us;
 	uint32_t off_on_delay_us;
 };
@@ -47,14 +49,14 @@ static int pd_on_domain_visitor(const struct device *dev, void *context)
 
 #endif
 
-static int pd_gpio_pm_action(const struct device *dev,
-			     enum pm_device_action action)
+static int pd_gpio_pm_action(const struct device *dev, enum pm_device_action action)
 {
 #ifdef CONFIG_PM_DEVICE_POWER_DOMAIN
 	struct pd_visitor_context context = {.domain = dev};
 #endif
 	const struct pd_gpio_config *cfg = dev->config;
 	struct pd_gpio_data *data = dev->data;
+	const struct gpio_dt_spec *gpio;
 	int rc = 0;
 
 	/* Validate that blocking API's can be used */
@@ -69,6 +71,14 @@ static int pd_gpio_pm_action(const struct device *dev,
 		k_sleep(sys_timepoint_timeout(data->earliest_next_boot));
 		/* Switch power on */
 		gpio_pin_set_dt(&cfg->enable, 1);
+		/* Enable all follower GPIOs */
+		for (int i = 0; i < cfg->num_followers; i++) {
+			/* Set pin back to physical high value */
+			gpio = &cfg->followers[i];
+			gpio_pin_set_raw(gpio->port, gpio->pin, 1);
+			LOG_DBG("%s:%02d active", cfg->followers[i].port->name,
+				cfg->followers[i].pin);
+		}
 		LOG_INF("%s is now ON", dev->name);
 		/* Wait for domain to come up */
 		k_sleep(K_USEC(cfg->startup_delay_us));
@@ -84,6 +94,14 @@ static int pd_gpio_pm_action(const struct device *dev,
 		context.action = PM_DEVICE_ACTION_TURN_OFF;
 		(void)device_supported_foreach(dev, pd_on_domain_visitor, &context);
 #endif
+		/* Disable all follower GPIOs */
+		for (int i = 0; i < cfg->num_followers; i++) {
+			/* Set pin to physical low value */
+			gpio = &cfg->followers[i];
+			gpio_pin_set_raw(gpio->port, gpio->pin, 0);
+			LOG_DBG("%s:%02d inactive", cfg->followers[i].port->name,
+				cfg->followers[i].pin);
+		}
 		/* Switch power off */
 		gpio_pin_set_dt(&cfg->enable, 0);
 		LOG_INF("%s is now OFF", dev->name);
@@ -91,6 +109,14 @@ static int pd_gpio_pm_action(const struct device *dev,
 		data->earliest_next_boot = sys_timepoint_calc(K_USEC(cfg->off_on_delay_us));
 		break;
 	case PM_DEVICE_ACTION_TURN_ON:
+		/* Ensure follower GPIOs are disabled */
+		for (int i = 0; i < cfg->num_followers; i++) {
+			/* Set pin to physical low value */
+			gpio = &cfg->followers[i];
+			gpio_pin_set_raw(gpio->port, gpio->pin, 0);
+			LOG_DBG("%s:%02d inactive", cfg->followers[i].port->name,
+				cfg->followers[i].pin);
+		}
 		/* Actively control the enable pin now that the device is powered */
 		gpio_pin_configure_dt(&cfg->enable, GPIO_OUTPUT_INACTIVE);
 		LOG_DBG("%s is OFF and powered", dev->name);
@@ -123,17 +149,27 @@ static int pd_gpio_init(const struct device *dev)
 	return pm_device_driver_init(dev, pd_gpio_pm_action);
 }
 
-#define POWER_DOMAIN_DEVICE(id)							\
-	static const struct pd_gpio_config pd_gpio_##id##_cfg = {		\
-		.enable = GPIO_DT_SPEC_INST_GET(id, enable_gpios),		\
-		.startup_delay_us = DT_INST_PROP(id, startup_delay_us),		\
-		.off_on_delay_us = DT_INST_PROP(id, off_on_delay_us),		\
-	};									\
-	static struct pd_gpio_data pd_gpio_##id##_data;				\
-	PM_DEVICE_DT_INST_DEFINE(id, pd_gpio_pm_action);			\
-	DEVICE_DT_INST_DEFINE(id, pd_gpio_init, PM_DEVICE_DT_INST_GET(id),	\
-			      &pd_gpio_##id##_data, &pd_gpio_##id##_cfg,	\
-			      POST_KERNEL, CONFIG_POWER_DOMAIN_GPIO_INIT_PRIORITY,	\
-			      NULL);
+#define _INST_RAW_FOLLOWERS(id)                                                                    \
+	DT_INST_FOREACH_PROP_ELEM_SEP(id, raw_follower_gpios, GPIO_DT_SPEC_GET_BY_IDX, (, ))
+
+/* Generate array of raw follower GPIOs */
+#define RAW_FOLLOWERS(id, name)                                                                    \
+	static const struct gpio_dt_spec name[] = {COND_CODE_1(                                    \
+		DT_INST_NODE_HAS_PROP(id, raw_follower_gpios), (_INST_RAW_FOLLOWERS(id)), ())}
+
+#define POWER_DOMAIN_DEVICE(id)                                                                    \
+	RAW_FOLLOWERS(id, followers##id);                                                          \
+	static const struct pd_gpio_config pd_gpio_##id##_cfg = {                                  \
+		.enable = GPIO_DT_SPEC_INST_GET(id, enable_gpios),                                 \
+		.followers = followers##id,                                                        \
+		.num_followers = ARRAY_SIZE(followers##id),                                        \
+		.startup_delay_us = DT_INST_PROP(id, startup_delay_us),                            \
+		.off_on_delay_us = DT_INST_PROP(id, off_on_delay_us),                              \
+	};                                                                                         \
+	static struct pd_gpio_data pd_gpio_##id##_data;                                            \
+	PM_DEVICE_DT_INST_DEFINE(id, pd_gpio_pm_action);                                           \
+	DEVICE_DT_INST_DEFINE(id, pd_gpio_init, PM_DEVICE_DT_INST_GET(id), &pd_gpio_##id##_data,   \
+			      &pd_gpio_##id##_cfg, POST_KERNEL,                                    \
+			      CONFIG_POWER_DOMAIN_GPIO_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(POWER_DOMAIN_DEVICE)
