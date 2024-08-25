b2c695e30c98dc3a46b23b80f27834849c7ff56d
Jordan Yates <jordan@embeint.com>
2024-08-25 10:45:48 +1000

    Revert "sd: sd_utils: optional reset function for sd_retry"
    This reverts commit 550a64b0ef158e6fdf0cee12e216f4587a8de1d9.

--- subsys/sd/sd.c
+++ subsys/sd/sd.c
@@ -94,8 +94,14 @@ static int sd_common_init(struct sd_card *card)
 {
 	int ret;
 
-	/* Perform voltage check using SD CMD8, reset using CMD0 on errors */
-	ret = sd_retry(sd_send_interface_condition, sd_idle, card, CONFIG_SD_RETRY_COUNT);
+	/* Reset card with CMD0 */
+	ret = sd_idle(card);
+	if (ret) {
+		LOG_ERR("Card error on CMD0");
+		return ret;
+	}
+	/* Perform voltage check using SD CMD8 */
+	ret = sd_retry(sd_send_interface_condition, card, CONFIG_SD_RETRY_COUNT);
 	if (ret == -ETIMEDOUT) {
 		LOG_INF("Card does not support CMD8, assuming legacy card");
 		return sd_idle(card);
