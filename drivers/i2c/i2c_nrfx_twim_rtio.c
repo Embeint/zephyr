7020f1280b526bb6d095f936858901d544a1ae5c
Jordan Yates <jordan@embeint.com>
2024-08-27 20:53:34 +1000

    i2c: nrfx_twim_common: recover bus on init
    Recover any transient bus errors that may have been caused by the reboot
    on initialisation. Add a devicetree property that skips the bus recovery
    on boot, as recovery on the Thingy53 has been observed to fail due to
    the unpowered sensors on the bus.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/i2c/i2c_nrfx_twim_rtio.c
+++ drivers/i2c/i2c_nrfx_twim_rtio.c
@@ -283,6 +283,7 @@ static int i2c_nrfx_twim_rtio_deinit(const struct device *dev)
 				.pcfg = PINCTRL_DT_DEV_CONFIG_GET(I2C(idx)),                       \
 				IF_ENABLED(USES_MSG_BUF(idx), (.msg_buf = MSG_BUF_SYM(idx),))      \
 				.max_transfer_size = MAX_TRANSFER_SIZE(idx),                       \
+				.no_boot_recover = DT_PROP(I2C(idx), zephyr_no_boot_recover),      \
 			},                                                                         \
 		.ctx = &_i2c##idx##_twim_rtio,                                                     \
 	};                                                                                         \
