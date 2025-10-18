18f0d5934a30e4fb4d6c1d4f42348351406aae26
Jordan Yates <jordan@embeint.com>
2025-10-18 16:24:36 +1000

    modem: at_user_pipe: attach on claim
    Update the AT user pipe API so that the initialisation does not require
    the modem chat instance. This enables sharing the user pipe abstraction
    between multiple users at runtime.
    
    One consequence of this is that `modem_chat_attach` is deferred until
    the user calls `modem_at_user_pipe_claim`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/modem/modem_at_user_pipe.c
+++ drivers/modem/modem_at_user_pipe.c
@@ -13,8 +13,9 @@
 #define AT_UTIL_MODEM_NODE    DT_ALIAS(modem)
 #define AT_UTIL_PIPELINK_NAME _CONCAT(user_pipe_, CONFIG_MODEM_AT_USER_PIPE_IDX)
 
-#define AT_UTIL_STATE_ATTACHED_BIT       0
-#define AT_UTIL_STATE_SCRIPT_RUNNING_BIT 1
+#define AT_UTIL_STATE_OPENED_BIT                0
+#define AT_UTIL_STATE_PIPE_CLAIMED_BIT          1
+#define AT_UTIL_STATE_PIPE_AT_ECHO_DISABLED_BIT 2
 
 MODEM_PIPELINK_DT_DECLARE(AT_UTIL_MODEM_NODE, AT_UTIL_PIPELINK_NAME);
 
@@ -22,8 +23,6 @@ static struct modem_pipelink *at_util_pipelink =
 	MODEM_PIPELINK_DT_GET(AT_UTIL_MODEM_NODE, AT_UTIL_PIPELINK_NAME);
 
 static struct k_work at_util_open_pipe_work;
-static struct k_work at_util_attach_chat_work;
-static struct k_work at_util_release_chat_work;
 static struct modem_chat *at_util_chat;
 static atomic_t at_util_state;
 
@@ -37,9 +36,14 @@ static void at_util_pipe_callback(struct modem_pipe *pipe, enum modem_pipe_event
 	switch (event) {
 	case MODEM_PIPE_EVENT_OPENED:
 		LOG_INF("pipe opened");
-		k_work_submit(&at_util_attach_chat_work);
+		atomic_set_bit(&at_util_state, AT_UTIL_STATE_OPENED_BIT);
 		break;
-
+	case MODEM_PIPE_EVENT_CLOSED:
+		LOG_INF("pipe closed");
+		atomic_clear_bit(&at_util_state, AT_UTIL_STATE_OPENED_BIT);
+		atomic_clear_bit(&at_util_state, AT_UTIL_STATE_PIPE_AT_ECHO_DISABLED_BIT);
+		modem_at_user_pipe_release();
+		break;
 	default:
 		break;
 	}
@@ -56,11 +60,6 @@ void at_util_pipelink_callback(struct modem_pipelink *link, enum modem_pipelink_
 		k_work_submit(&at_util_open_pipe_work);
 		break;
 
-	case MODEM_PIPELINK_EVENT_DISCONNECTED:
-		LOG_INF("pipe disconnected");
-		k_work_submit(&at_util_release_chat_work);
-		break;
-
 	default:
 		break;
 	}
@@ -68,13 +67,14 @@ void at_util_pipelink_callback(struct modem_pipelink *link, enum modem_pipelink_
 
 static void at_util_open_pipe_handler(struct k_work *work)
 {
+	struct modem_pipe *pipe = modem_pipelink_get_pipe(at_util_pipelink);
 	ARG_UNUSED(work);
 
 	LOG_INF("opening pipe");
 
-	modem_pipe_attach(modem_pipelink_get_pipe(at_util_pipelink), at_util_pipe_callback, NULL);
+	modem_pipe_attach(pipe, at_util_pipe_callback, NULL);
 
-	modem_pipe_open_async(modem_pipelink_get_pipe(at_util_pipelink));
+	modem_pipe_open_async(pipe);
 }
 
 #ifdef CONFIG_MODEM_CMUX_DLCI_AT_ECHO_DEDICATED
@@ -87,56 +87,48 @@ MODEM_CHAT_SCRIPT_CMDS_DEFINE(
 MODEM_CHAT_SCRIPT_DEFINE(ate_disable_script, ate_disable_script_cmds, abort_matches, NULL, 1);
 #endif /* CONFIG_MODEM_CMUX_DLCI_AT_ECHO_DEDICATED */
 
-static void at_util_attach_chat_handler(struct k_work *work)
+int modem_at_user_pipe_claim(struct modem_chat *chat)
 {
 	struct modem_pipe *pipe = modem_pipelink_get_pipe(at_util_pipelink);
 
-	ARG_UNUSED(work);
-
-	modem_chat_attach(at_util_chat, pipe);
-
-#ifdef CONFIG_MODEM_CMUX_DLCI_AT_ECHO_DEDICATED
-	LOG_DBG("disabling echo");
-	(void)modem_chat_run_script_async(at_util_chat, &ate_disable_script);
-#endif /* CONFIG_MODEM_CMUX_DLCI_AT_ECHO_DEDICATED */
-
-	atomic_set_bit(&at_util_state, AT_UTIL_STATE_ATTACHED_BIT);
-	LOG_INF("chat attached");
-}
-
-static void at_util_release_chat_handler(struct k_work *work)
-{
-	ARG_UNUSED(work);
-
-	modem_chat_release(at_util_chat);
-	atomic_clear_bit(&at_util_state, AT_UTIL_STATE_ATTACHED_BIT);
-	LOG_INF("chat released");
-}
-
-void modem_at_user_pipe_init(struct modem_chat *chat)
-{
-	at_util_chat = chat;
-	/* Initialise workers and setup callbacks */
-	k_work_init(&at_util_open_pipe_work, at_util_open_pipe_handler);
-	k_work_init(&at_util_attach_chat_work, at_util_attach_chat_handler);
-	k_work_init(&at_util_release_chat_work, at_util_release_chat_handler);
-	modem_pipelink_attach(at_util_pipelink, at_util_pipelink_callback, NULL);
-}
-
-int modem_at_user_pipe_claim(void)
-{
-	if (!atomic_test_bit(&at_util_state, AT_UTIL_STATE_ATTACHED_BIT)) {
+	if (!atomic_test_bit(&at_util_state, AT_UTIL_STATE_OPENED_BIT)) {
 		return -EPERM;
 	}
 
-	if (atomic_test_and_set_bit(&at_util_state, AT_UTIL_STATE_SCRIPT_RUNNING_BIT)) {
+	if (atomic_test_and_set_bit(&at_util_state, AT_UTIL_STATE_PIPE_CLAIMED_BIT)) {
 		return -EBUSY;
 	}
 
+	__ASSERT_NO_MSG(at_util_chat == NULL);
+	at_util_chat = chat;
+	modem_chat_attach(at_util_chat, pipe);
+
+#ifdef CONFIG_MODEM_CMUX_DLCI_AT_ECHO_DEDICATED
+	if (!atomic_test_and_set_bit(&at_util_state, AT_UTIL_STATE_PIPE_AT_ECHO_DISABLED_BIT)) {
+		/* AT echo has not yet been disabled */
+		LOG_DBG("disabling echo");
+		(void)modem_chat_run_script(at_util_chat, &ate_disable_script);
+	}
+#endif /* CONFIG_MODEM_CMUX_DLCI_AT_ECHO_DEDICATED */
+	LOG_DBG("chat attached");
 	return 0;
 }
 
 void modem_at_user_pipe_release(void)
 {
-	atomic_clear_bit(&at_util_state, AT_UTIL_STATE_SCRIPT_RUNNING_BIT);
+	if (atomic_test_and_clear_bit(&at_util_state, AT_UTIL_STATE_PIPE_CLAIMED_BIT)) {
+		modem_chat_release(at_util_chat);
+		at_util_chat = NULL;
+		LOG_DBG("chat released");
+	}
 }
+
+int modem_at_user_pipe_init(void)
+{
+	/* Initialise workers and setup callbacks */
+	k_work_init(&at_util_open_pipe_work, at_util_open_pipe_handler);
+	modem_pipelink_attach(at_util_pipelink, at_util_pipelink_callback, NULL);
+	return 0;
+}
+
+SYS_INIT(modem_at_user_pipe_init, APPLICATION, 0);
