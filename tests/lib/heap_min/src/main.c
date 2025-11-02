99a228b3c5d4dd1a599c50c7304897cb38a376a4
Jordan Yates <jordan@embeint.com>
2025-11-02 21:39:07 +1000

    tests: lib: heap_min: measure heap overhead
    Add a testcase that outputs the overhead of heaps declared multiple
    different ways, by printing the largest single allocation that can be
    made from the heap.
    
    ```
    START - test_heap_overhead
    Base |  Raw | Z_HEAP_MIN_SIZE | Z_HEAP_OVERHEAD
      64 |   12 |              52 |              60
     128 |   68 |             116 |             132
     256 |  196 |             236 |             252
     512 |  444 |             492 |             516
    1024 |  956 |             996 |            1020
    2048 | 1972 |            2020 |            2052
    ```
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/lib/heap_min/src/main.c
+++ tests/lib/heap_min/src/main.c
@@ -35,4 +35,44 @@ ZTEST(lib_heap_min, test_heap_min_size)
 	sys_heap_free(&heap, mem);
 }
 
+static int find_maximum_single_allocation(struct sys_heap *heap, int base_size)
+{
+	void *mem;
+
+	for (int i = base_size + 16; i > 0; i--) {
+		mem = sys_heap_alloc(heap, i);
+		if (mem) {
+			sys_heap_free(heap, mem);
+			return i;
+		}
+	}
+	return 0;
+}
+
+ZTEST(lib_heap_min, test_heap_overhead)
+{
+	int16_t base_heap_sizes[] = {64, 128, 256, 512, 1024, 2048};
+	struct sys_heap heap;
+	int base;
+	int no_overhead;
+	int min_overhead;
+	int macro_overhead;
+
+	TC_PRINT("Base |  Raw | Z_HEAP_MIN_SIZE | Z_HEAP_OVERHEAD\n");
+
+	for (int i = 0; i < ARRAY_SIZE(base_heap_sizes); i++) {
+		base = base_heap_sizes[i];
+
+		sys_heap_init(&heap, heapmem, base);
+		no_overhead = find_maximum_single_allocation(&heap, base);
+		sys_heap_init(&heap, heapmem, base + Z_HEAP_MIN_SIZE);
+		min_overhead = find_maximum_single_allocation(&heap, base);
+		sys_heap_init(&heap, heapmem, base + Z_HEAP_OVERHEAD(base));
+		macro_overhead = find_maximum_single_allocation(&heap, base);
+
+		TC_PRINT("%4u | %4u | %15u | %15u\n", base, no_overhead, min_overhead,
+			 macro_overhead);
+	}
+}
+
 ZTEST_SUITE(lib_heap_min, NULL, NULL, NULL, NULL, NULL);
