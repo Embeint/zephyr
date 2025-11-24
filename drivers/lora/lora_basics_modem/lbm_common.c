b8294e8e56abbc5f25b74e78c7732edcbd289014
Jordan Yates <jordan@embeint.com>
2025-11-24 16:22:05 +1000

    lora: preamble and header detection callbacks
    Add the option to receive callbacks when the LoRa modem detects a valid
    preamble or packet header. This can be used to detect whether the modem
    is in the middle of receiving a packet, allowing the user to possibly
    defer a mode switch (to TX or IDLE) if it is. This is useful due to the
    generally long packet durations on LoRa.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/lora/lora_basics_modem/lbm_common.c
+++ drivers/lora/lora_basics_modem/lbm_common.c
@@ -11,8 +11,8 @@
 
 /* LoRa interrupts from the RAL library */
 #define RAL_IRQ_LORA                                                                               \
-	RAL_IRQ_TX_DONE | RAL_IRQ_RX_DONE | RAL_IRQ_RX_HDR_ERROR | RAL_IRQ_RX_CRC_ERROR |          \
-		RAL_IRQ_CAD_DONE | RAL_IRQ_CAD_OK
+	(RAL_IRQ_TX_DONE | RAL_IRQ_RX_DONE | RAL_IRQ_RX_HDR_ERROR | RAL_IRQ_RX_CRC_ERROR |         \
+	 RAL_IRQ_CAD_DONE | RAL_IRQ_CAD_OK)
 
 LOG_MODULE_REGISTER(lbm_driver, CONFIG_LORA_LOG_LEVEL);
 
@@ -418,10 +418,15 @@ int lbm_lora_recv_async(const struct device *dev, const struct lora_recv_async_c
 {
 	const struct lbm_lora_config_common *config = dev->config;
 	struct lbm_lora_data_common *data = dev->data;
+	ral_irq_t irqs = RAL_IRQ_LORA;
 	ral_status_t status;
 
 	/* Cancel ongoing reception */
 	if (cb == NULL) {
+		if (data->rx_state.async.preamble_detected || data->rx_state.async.header_valid) {
+			/* Reset IRQ state */
+			(void)ral_set_dio_irq_params(&config->ralf.ral, irqs);
+		}
 		if (!modem_release(dev)) {
 			/* Not receiving or already being stopped */
 			return -EINVAL;
@@ -438,13 +443,28 @@ int lbm_lora_recv_async(const struct device *dev, const struct lora_recv_async_c
 		return -EBUSY;
 	}
 
+	/* Configure interrupts for desired events */
+	if (cb->preamble_detected) {
+		irqs |= RAL_IRQ_RX_PREAMBLE_DETECTED;
+	}
+	if (cb->header_valid) {
+		irqs |= RAL_IRQ_RX_HDR_OK;
+	}
+	if (irqs != RAL_IRQ_LORA) {
+		status = ral_set_dio_irq_params(&config->ralf.ral, irqs);
+		if (status != RAL_STATUS_OK) {
+			modem_release(dev);
+			LOG_ERR("RAL DIO init failure (%d)", status);
+			return -EIO;
+		}
+	}
+
 	/* Configure modem for asynchronous RX */
 	lbm_driver_antenna_configure(dev, MODE_RX_ASYNC);
 	data->modem_mode = MODE_RX_ASYNC;
 
 	/* Store user state */
-	data->rx_state.async.rx_cb = cb->recv;
-	data->rx_state.async.user_data = cb->user_data;
+	data->rx_state.async = *cb;
 
 	/* Start the reception in continuous mode */
 	status = ral_set_rx(&config->ralf.ral, RAL_RX_TIMEOUT_CONTINUOUS_MODE);
@@ -565,8 +585,8 @@ static void op_done_async_rx(const struct device *dev)
 		       : pkt_status.signal_rssi_pkt_in_dbm;
 
 	/* Run the user callback */
-	data->rx_state.async.rx_cb(dev, rx_buffer, size, rssi, pkt_status.snr_pkt_in_db,
-				   data->rx_state.async.user_data);
+	data->rx_state.async.recv(dev, rx_buffer, size, rssi, pkt_status.snr_pkt_in_db,
+				  data->rx_state.async.user_data);
 }
 
 static void op_done_work_handler(struct k_work *work)
@@ -590,6 +610,19 @@ static void op_done_work_handler(struct k_work *work)
 	error_irq = irq_state & (RAL_IRQ_RX_TIMEOUT | RAL_IRQ_RX_HDR_ERROR | RAL_IRQ_RX_CRC_ERROR);
 
 	LOG_DBG("%d: IRQ %08X", data->modem_mode, irq_state);
+
+	if ((irq_state & RAL_IRQ_RX_PREAMBLE_DETECTED) && data->rx_state.async.preamble_detected) {
+		LOG_DBG("Preamble detected IRQ");
+		/* Run the user callback */
+		data->rx_state.async.preamble_detected(dev, data->rx_state.async.user_data);
+		return;
+	}
+	if ((irq_state & RAL_IRQ_RX_HDR_OK) && data->rx_state.async.header_valid) {
+		LOG_DBG("Header valid IRQ");
+		/* Run the user callback */
+		data->rx_state.async.header_valid(dev, data->rx_state.async.user_data);
+		return;
+	}
 
 	switch (data->modem_mode) {
 	case MODE_SLEEP:
