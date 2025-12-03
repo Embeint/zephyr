91d86a9c8f8b5065e60bae3a3e8ab538401d28b7
Jordan Yates <jordan@embeint.com>
2025-12-03 14:03:45 +1000

    i2c/i3c: rtio: explicit zeroing of SQE flags
    Explcitly zero the flags of the SQE obtained from `rtio_sqe_acquire`
    when the `rtio_sqe_prep_*` helpers aren't used (the helpers memset the
    entire struct to 0).
    
    The flags must be reset to ensure the `RTIO_SQE_TRANSACTION` and
    `RTIO_SQE_CHAINED` are not retained from a previous SQE.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/i2c/i2c_rtio.c
+++ drivers/i2c/i2c_rtio.c
@@ -213,6 +213,7 @@ int i2c_rtio_configure(struct i2c_rtio *ctx, uint32_t i2c_config)
 	}
 
 	sqe->op = RTIO_OP_I2C_CONFIGURE;
+	sqe->flags = 0;
 	sqe->iodev = iodev;
 	sqe->i2c_config = i2c_config;
 
@@ -245,6 +246,7 @@ int i2c_rtio_recover(struct i2c_rtio *ctx)
 	}
 
 	sqe->op = RTIO_OP_I2C_RECOVER;
+	sqe->flags = 0;
 	sqe->iodev = iodev;
 
 	rtio_submit(r, 1);
@@ -276,6 +278,7 @@ int i2c_rtio_sda_toggle(struct i2c_rtio *ctx, uint8_t cycles)
 	}
 
 	sqe->op = RTIO_OP_I2C_SDA_TOGGLE;
+	sqe->flags = 0;
 	sqe->iodev = iodev;
 	sqe->cycles = cycles;
 
