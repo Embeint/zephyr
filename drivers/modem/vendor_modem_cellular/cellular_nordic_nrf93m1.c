d32d65803ed85a4084fa7d2fb8019c29039ed48f
Jordan Yates <jordan@embeint.com>
2026-09-12 15:10:50 +1000

    modem: cellular: nrf93m1: remove unsupported user channel
    The nRF93M1 does not appear to support 4 DLCI channels. Any attempt to
    open a DLCI channel with index over 3 results in a DM response with the
    F-bit set, indicating the modem is unwilling to open the channel.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/modem/vendor_modem_cellular/cellular_nordic_nrf93m1.c
+++ drivers/modem/vendor_modem_cellular/cellular_nordic_nrf93m1.c
@@ -148,7 +148,7 @@ static const struct modem_cellular_vendor_config nrf93m1_vendor = {
 		.ppp = &MODEM_CELLULAR_INST_NAME(ppp, inst),                                       \
 	};                                                                                         \
                                                                                                    \
-	MODEM_CELLULAR_DEFINE_AND_INIT_USER_PIPES(inst, (user_pipe_0, 3), (user_pipe_1, 4))        \
+	MODEM_CELLULAR_DEFINE_AND_INIT_USER_PIPES(inst, (user_pipe_0, 3))                          \
                                                                                                    \
 	MODEM_CELLULAR_DEFINE_INSTANCE(inst, &nrf93m1_vendor)
 
